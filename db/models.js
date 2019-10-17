30b0083395167cf4657e0841de9be54a7f01c628
ThanksXly <328704719@qq.com>
2019-10-17 17:48:39 +0800

    '我也不知道为啥只有这个文件更新了'

--- db/models.js
+++ db/models.js
@@ -12,7 +12,7 @@
  */
 // 1. 连接数据库
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/guigu_zhipin');
+mongoose.connect('mongodb://localhost:27017/x');
 const conn = mongoose.connection;
 conn.on('connected', function () {
   console.log('数据库连接成功!')
